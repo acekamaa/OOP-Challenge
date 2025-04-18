992738d6a971caf345ebccdfb15f1c9317776b4a
acekamaa <kvnkamaa@gmail.com>
2025-04-18 21:59:29 +0300

    Create main.py by group number 520

---
+++ main.py
@@ -0,0 +1,11 @@
+# call a module - pet.py
+import pet as p
+
+# create a pet object
+my_pet = p.Pet("Buddy", 5, 7, 8, "roll over")
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("sit")
+my_pet.show_tricks()
+my_pet.get_status()
